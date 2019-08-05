56a311e49aa4bf0aae95a10a0679619a68e8f5b0
Janet Warren <jewljbl13@aol.com>
2019-08-05 16:38:33 -0400

    build profile top and profile about components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,10 +1,12 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profileActions';
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 
 const Profile = ({
   getProfileById,
@@ -14,15 +16,32 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
-  return <Fragment>
-      {profile === null || loading ? <Spinner /> :
-      <Fragment>
-          <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
-          {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && <Link to ='edit-profile' className = ' btn btn-dark'>Edit Profile</Link>}
-      </Fragment>}
-  </Fragment>;
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to='/profiles' className='btn btn-light'>
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='edit-profile' className=' btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+             <div class="profile-grid my-1">
+                 <ProfileTop profile={profile} />
+                 <ProfileAbout profile={profile} />
+             </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profile.propTypes = {
