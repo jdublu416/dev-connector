64e91b1a8fcdc898edff98a0bcbfaac159eda130
Janet Warren <jewljbl13@aol.com>
2019-08-09 19:21:47 -0400

    delete post functionality

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -26,6 +26,7 @@ export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 
 // Post Actions
 export const GET_POSTS = 'GET_POSTS';
+export const DELETE_POST = 'DELETE_POST';
 export const POST_ERROR = 'POST_ERROR';
 export const UPDATE_LIKES = 'UPDATE_LIKES';
 
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
-import { addLike, removeLike } from '../../actions/postActions';
+import { addLike, removeLike, deletePost } from '../../actions/postActions';
 
 const PostItem = ({
   addLike,
   removeLike,
+  deletePost,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date }
 }) => (
@@ -50,7 +51,7 @@ const PostItem = ({
         )}
       </Link>
       {!auth.loading && user === auth.user._id && (
-        <button type='button' className='btn btn-danger'>
+        <button type='button' className='btn btn-danger' onClick={e => deletePost(_id)} >
           <i className='fas fa-times' />
         </button>
       )}
@@ -60,7 +61,10 @@ const PostItem = ({
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -69,5 +73,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { addLike, removeLike }
+  { addLike, removeLike, deletePost }
 )(PostItem);
